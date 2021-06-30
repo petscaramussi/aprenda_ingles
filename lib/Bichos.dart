8110ac50d5228285ede18a1a3e4b877a8463a28a
Peterson <44592787+petscaramussi@users.noreply.github.com>
2021-06-29 22:56:03 -0300

    add assets

--- lib/Bichos.dart
+++ lib/Bichos.dart
@@ -12,12 +12,50 @@ class _BichosState extends State<Bichos> {
   Widget build(BuildContext context) {
     return GridView.count(
       crossAxisCount: 2,
+      childAspectRatio: MediaQuery.of(context).size.aspectRatio * 2,
       children: <Widget>[
         GestureDetector(
           onTap: (){
             
           },
-        )
+
+          child: Image.asset("assets/imagens/cao.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+            
+          },
+
+          child: Image.asset("assets/imagens/gato.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+            
+          },
+
+          child: Image.asset("assets/imagens/leao.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+            
+          },
+
+          child: Image.asset("assets/imagens/macaco.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+            
+          },
+
+          child: Image.asset("assets/imagens/ovelha.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+            
+          },
+
+          child: Image.asset("assets/imagens/vaca.png"),
+        ),
       ],
     );
   }
